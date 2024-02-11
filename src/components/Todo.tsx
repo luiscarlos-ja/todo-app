8276bad409df5fb40d10ba216960578df733c1a9
Luis Carlos <luiscarlos_ja@outlook.com>
2024-02-11 15:34:22 -0600

    Refactor Todo component and add editing functionality

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,30 +1,72 @@
+import { useEffect, useRef, useState } from 'react'
 import { useTodos } from '../hooks/useTodos'
 import { type Todo as TodoType } from '../types'
 
 interface Props {
   todo: TodoType
+  isEditing: string
+  setIsEditing: (id: string) => void
 }
 
-export const Todo: React.FC<Props> = ({ todo }) => {
-  const { handleCompleted, handleRemove } = useTodos()
+export const Todo: React.FC<Props> = ({ todo, isEditing, setIsEditing }) => {
+  const { handleCompleted, handleRemove, handleUpdateTitle } = useTodos()
+  const [editedTitle, setEditedTitle] = useState(todo.title)
+  const inputEditTitle = useRef<HTMLInputElement>(null)
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') {
+      setEditedTitle(editedTitle.trim())
+
+      if (editedTitle !== todo.title) {
+        handleUpdateTitle({ id: todo.id, title: editedTitle })
+      }
+
+      if (editedTitle === '') handleRemove(todo.id)
+
+      setIsEditing('')
+    }
+
+    if (e.key === 'Escape') {
+      setEditedTitle(todo.title)
+      setIsEditing('')
+    }
+  }
+
+  useEffect(() => {
+    inputEditTitle.current?.focus()
+  }, [isEditing])
 
   return (
-    <div className="view">
+    <>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => {
+            handleCompleted(todo.id, e.target.checked)
+          }}
+        />
+        <label>{todo.title}</label>
+        <button
+          className="destroy"
+          onClick={() => {
+            handleRemove(todo.id)
+          }}
+        />
+      </div>
       <input
-        className="toggle"
-        type="checkbox"
-        checked={todo.completed}
+        className="edit"
+        value={editedTitle}
         onChange={(e) => {
-          handleCompleted(todo.id, e.target.checked)
+          setEditedTitle(e.target.value)
         }}
-      />
-      <label>{todo.title}</label>
-      <button
-        className="destroy"
-        onClick={() => {
-          handleRemove(todo.id)
+        onKeyDown={handleKeyDown}
+        onBlur={() => {
+          setIsEditing('')
         }}
+        ref={inputEditTitle}
       />
-    </div>
+    </>
   )
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import { useTodos } from '../hooks/useTodos'
 import { Todo } from './Todo'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 export const Todos: React.FC = () => {
   const { todos } = useTodos()
+  const [parent] = useAutoAnimate()
+  const [isEditing, setIsEditing] = useState('')
 
   return (
-    <ul className="todo-list">
+    <ul className="todo-list" ref={parent}>
       {todos?.map((todo) => (
-        <li key={todo.id} className={`${todo.completed ? 'completed' : ''}`}>
-          <Todo todo={todo} />
+        <li
+          key={todo.id}
+          className={`
+        ${todo.completed ? 'completed' : ''}
+        ${isEditing === todo.id ? 'editing' : ''}
+      `}
+          onDoubleClick={() => {
+            setIsEditing(todo.id)
+          }}
+        >
+          <Todo todo={todo} isEditing={isEditing} setIsEditing={setIsEditing} />
         </li>
       ))}
     </ul>
